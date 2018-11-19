7c3f29f03c05eaee5d44df750871c4c61b872674
Abel González Rodriguez <abelgr45@gmail.com>
2018-11-18 20:52:05 -0400

    se realizo la tarea

---
+++ source/repos/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class ClockByReymond
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.txttime = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hora :";
+            // 
+            // txttime
+            // 
+            this.txttime.Location = new System.Drawing.Point(54, 27);
+            this.txttime.Name = "txttime";
+            this.txttime.Size = new System.Drawing.Size(153, 20);
+            this.txttime.TabIndex = 1;
+            // 
+            // ClockByReymond
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 110);
+            this.Controls.Add(this.txttime);
+            this.Controls.Add(this.label1);
+            this.Name = "ClockByReymond";
+            this.Text = "Clock by Reymond";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txttime;
+    }
+}
+
---
+++ source/repos/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class ClockByReymond : Form
+    {
+        public ClockByReymond()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txttime.Text = DateTime.Now.ToString("HH:MM:ss");
+        }
+    }
+}
